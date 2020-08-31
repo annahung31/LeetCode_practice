ef646c02fb42013d08d3d78f158bf7e1511abf27
annahung31 <fbiannahung@gmail.com>
2020-08-31 18:47:19 +0800

    init

---
+++ ADA2018/hw2-3_1/hw2-3_1.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n,m;
+    vector<int> price;
+
+
+    vector<int> price;
+    for (i=0; i<n; i++){
+        if (i ==0){
+            cin >> value;
+            price.push_back(value);
+            }
+}
