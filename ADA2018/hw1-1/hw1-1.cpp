7cc244e9ced8eafc256578caa1db4bcd7e332729
annahung31 <fbiannahung@gmail.com>
2020-08-31 18:28:53 +0800

    init

---
+++ ADA2018/hw1-1/hw1-1.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <vector>
+#include <iostream>
+using namespace std;
+int main(){
+    int n, i, value;
+    int minPrice=0;
+    int maxPro = 0;
+    cin >> n;
+    
+    vector<int> price;
+    for (i=0; i<n; i++){
+        if (i ==0){
+            cin >> value;
+            price.push_back(value);
+            }
+        else{
+            cin >> value;
+            value += price[i-1];
+            price.push_back(value);
+
+        }
+        
+    }
+
+    for (i=0; i<price.size(); i++){
+        minPrice = min(minPrice, price[i]);
+        maxPro = max(maxPro, price[i]-minPrice);
+    }
+    cout << maxPro << "\n";
+    return maxPro;
+}
