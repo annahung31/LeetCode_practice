6af6807299222eecccd1ec33658a54519630e950
annahung31 <fbiannahung@gmail.com>
2020-09-03 15:06:02 +0800

    test

--- ADA2018/hw2-3_1/hw2-3_1.cpp
+++ ADA2018/hw2-3_1/hw2-3_1.cpp
@@ -1,18 +1,54 @@
 #include <stdio.h>
 #include <vector>
 #include <iostream>
-
+#include <bits/stdc++.h>
+#include <vector>
+#define MODULE 1000000007;
 using namespace std;
 
+int isLegal(int n){
+    int i, k=1;
+    int count[10] ={0};
+    
+    for (i=1; i<19; i++){
+        count[n/k % 10]++;
+        k *=10;
+    }
+    if (n % 33==0 && count[3]%3==0 && count[6]%3==0 && count[9]%3==0){
+        return 1;
+    }
+    return 0;
+
+    
+}
+
+
 int main(){
-    int n,m;
-    vector<int> price;
+    int n, m, i, j, value;
+    cin >> n ,m;
 
 
-    vector<int> price;
+    vector<int> row;
+    row.assign(n, 0);
+    vector< vector<int> > map;
+    map.assign(m, row);
+
     for (i=0; i<n; i++){
-        if (i ==0){
+        for (j=0; j<m; j++){
             cin >> value;
-            price.push_back(value);
-            }
+            map[i][j] = value;
+        }
+    }
+
+    for (i=0; i<n; i++){
+        for (j=0; j<m; j++){
+            
+            cout << map[i][j] << " ";
+        }
+    }
+
+    return 0;
+    //int LEGAL = isLegal(n);
+
+
 }
