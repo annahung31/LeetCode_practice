7715c330ec9a7a18b172757566e4c74f194439a4
annahung31 <fbiannahung@gmail.com>
2020-09-09 15:35:32 +0800

    v1

--- ADA2018/hw2-3_1/hw2-3_1.cpp
+++ ADA2018/hw2-3_1/hw2-3_1.cpp
@@ -3,10 +3,10 @@
 #include <iostream>
 #include <bits/stdc++.h>
 #include <vector>
-#define MODULE 1000000007;
+#define MOD 1000000007;
 using namespace std;
 
-int isLegal(int n){
+int checkSave(int n){
     int i, k=1;
     int count[10] ={0};
     
@@ -15,9 +15,9 @@ int isLegal(int n){
         k *=10;
     }
     if (n % 33==0 && count[3]%3==0 && count[6]%3==0 && count[9]%3==0){
-        return 1;
+        return 0;
     }
-    return 0;
+    return 1;
 
     
 }
@@ -32,6 +32,12 @@ int main(){
     vector< vector<int> > map;
     map.assign(n, row);
 
+    vector<int> dp_row;
+    dp_row.assign(m, 1);
+    vector< vector<int> > dp;
+    dp.assign(n, dp_row);
+
+
     for (i=0; i<n; i++){
         for (j=0; j<m; j++){
             cin >> value;
@@ -39,9 +45,21 @@ int main(){
         }
     }
 
+    for (i=1; i<n+1; i++){
+        for (j=1; j<m+1; j++){
+            int SAFETY = checkSave(map[i][j]);
 
-    return 0;
-    //int LEGAL = isLegal(n);
+            dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % MOD;
+            
+            if (SAFETY==0){
+                dp[i][j] = 0;
+            }
+            
+        }
+    }
+    
+    cout << dp[m - 1][n - 1] << endl;
+    return dp[m - 1][n - 1];
 
 
 }
