fa2d8054490f1e91ced86ffe81e07fdd43c26fad
annahung31 <fbiannahung@gmail.com>
2020-09-14 12:11:13 +0800

    final solu

--- ADA2018/hw2-1/main.cpp
+++ ADA2018/hw2-1/main.cpp
@@ -5,6 +5,28 @@
 using namespace std;
 
 
+int solu(int target){
+    int n=0, count=0, nLeft, leftCount;  //need n days to achieve target
+
+    while (true){
+        if (count + pow(2, n) < target){
+            count += pow(2, n);
+            n += 1;
+        }
+        else{
+            break;
+        }
+    }
+
+    nLeft = target - count;
+    if (nLeft > 0){
+        leftCount = __builtin_popcount(nLeft);
+        n += leftCount;
+    }
+
+    return n;
+}
+
 
 int main(){ 
     int n, i, value;
@@ -25,8 +47,8 @@ int main(){
     
 
     for (i=0; i<n; i++){
-        ans = log(targets[i]+1)/log(2); 
-        results[i] = ceil(ans);
+        ans = solu(targets[i]);
+        results[i] = ans;
         cout << results[i] << endl;
 
     }
