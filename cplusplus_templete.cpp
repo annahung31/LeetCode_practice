93e46146d6e495c33bd949cfbf4126a404f2da80
annahung31 <fbiannahung@gmail.com>
2020-09-17 10:36:14 +0800

    templete

---
+++ cplusplus_templete.cpp
@@ -0,0 +1,29 @@
+
+
+#include <bits/stdc++.h>
+#include<cmath>
+#include<vector>
+using namespace std;
+#define MOD 1000000007;
+#define INF 1000000000;
+
+int main(){ 
+    int n, i, j;
+    cin >> n;          
+
+
+    vector<int> a_vector;  
+    a_vector.assign(n, 0)
+
+    vector< vector<int> > vectorSet;
+    pointSet.assign(n, a_point);  
+
+    for (i=0; i<n; i++){
+        for (j=0; j<2; j++){
+            cin >> value;
+            pointSet[i][j] = value;
+        }
+    }
+
+    return 0; 
+}
