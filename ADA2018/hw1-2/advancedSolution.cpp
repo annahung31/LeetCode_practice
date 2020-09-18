69608bc136d5ccd1601a67511d2d0a1bf2744cb4
annahung31 <fbiannahung@gmail.com>
2020-09-18 11:33:28 +0800

    finish divide part

--- ADA2018/hw1-2/advancedSolution.cpp
+++ ADA2018/hw1-2/advancedSolution.cpp
@@ -5,49 +5,73 @@
 #include<algorithm>
 using namespace std;
 
-struct a_point{
-    int x_coor;
-    int y_coor;
-};
+// Driver function to sort the 2D vector 
+// on basis of a particular column 
+bool sortcol( const vector<int>& v1, 
+               const vector<int>& v2 ) { 
+ return v1[0] < v2[0]; 
+} 
 
 
-bool comp(const a_point &a, const a_point &b){
-    return a.x_coor < b.x_coor;
+int crossArea(vector< vector<int> > pointSet, int st_idx, int mid_idx, int ed_idx, int &totalArea){
+    totalArea += 1;
 }
 
 
-int solu(int n, vector< a_point > pointSet){
 
+int calArea(vector< vector<int> > pointSet, int st_idx, int ed_idx, int &totalArea){
+    
+    if (st_idx < ed_idx){
+        int j;
+        for (j=st_idx; j<=ed_idx; j++){
+            cout << pointSet[j][0] << ":" << pointSet[j][1]<< endl;
+        }
 
-    return totalArea;
+        // Divide
+        int mid_idx = (st_idx + ed_idx)/2;
+        calArea(pointSet, st_idx, mid_idx, totalArea);
+        calArea(pointSet, mid_idx+1, ed_idx, totalArea);
+
+        // conquer
+        crossArea(pointSet, st_idx, mid_idx, ed_idx, totalArea);
+        //totalArea = rightArea + leftArea + crossArea
+    }
+    
 }
-
-
 
 
 int main(){ 
-    int n, i, j, deltaX, deltaY, totalArea;
+    int n, i, j, value, deltaX, deltaY, totalArea=0;
     cin >> n;          
 
-    vector< a_point > pointSet;
-    
-    while (n--){
-        a_point onePoint;
-        int x_coor;
-        int y_coor;
-        cin >> x_coor >> y_coor;
-        onePoint.x_coor = x_coor;
-        onePoint.y_coor = y_coor;
-        pointSet.push_back(onePoint);
+    vector<int> a_point;
+    a_point.assign(2, 0);  
+
+    vector< vector<int> > pointSet;
+    pointSet.assign(n, a_point);  
+
+    for (i=0; i<n; i++){
+        for (j=0; j<2; j++){
+            cin >> value;
+            pointSet[i][j] = value;
+        }
     }
 
+    
+
     //sort the pointSet by x coordinate.
-    sort(pointSet.begin(),pointSet.end(),comp);
+    sort(pointSet.begin(), pointSet.end(), sortcol); 
+
+    cout << "after sorting: " << endl;
+    for (i=0; i<n; i++){
+        cout << pointSet[i][0] << " : " << pointSet[i][1] << endl;
+    
+    }
 
 
 
-
-    //totalArea = solu(n, pointSet);
+    calArea(pointSet, 0, n-1, totalArea);
+    
     //cout << totalArea << endl;
     //return totalArea; 
     return 0;
