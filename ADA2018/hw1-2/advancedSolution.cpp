1899f8b5f2a3a1697122be9e96d6221bf876248f
annahung31 <fbiannahung@gmail.com>
2020-09-21 12:05:56 +0800

    temp

--- ADA2018/hw1-2/advancedSolution.cpp
+++ ADA2018/hw1-2/advancedSolution.cpp
@@ -9,46 +9,71 @@ using namespace std;
 // on basis of a particular column 
 bool sortcol( const vector<int>& v1, 
                const vector<int>& v2 ) { 
- return v1[0] > v2[0]; 
+ return v1[0] < v2[0]; 
 } 
 
 
-int crossArea(vector< vector<int> > pointSet, int front_idx, int mid_idx, int end_idx, int &totalArea,
-                int &sigma_x, int &sigma_y, int &sigma_xy){
+int crossArea(vector< vector<int> > pointSet, int front_idx, int mid_idx, int end_idx, int &totalArea){
     
     vector< vector<int> > leftSet(pointSet.begin()+front_idx, pointSet.begin()+mid_idx+1);
     vector< vector<int> > rightSet(pointSet.begin()+mid_idx+1, pointSet.begin()+end_idx+1);
-    int i, leftIdx=0, rightIdx=0;
-
+    int i, j;
+    int sigma_x, sigma_y, sigma_xy, temp;
+    //from left to right
+    for (i=leftSet.size(); i >=0; i--){
+        sigma_x=0; sigma_y=0; sigma_xy=0; temp=0;
+        for (j=rightSet.size(); j >=0; j--){
+            if (rightSet[j][0] > leftSet[i][0] && rightSet[j][1] < leftSet[i][1]){
+                sigma_x+=rightSet[j][0];
+                sigma_y+=rightSet[j][1];
+                sigma_xy+=rightSet[j][0]*rightSet[j][1];
+            }
+        }
+        temp = leftSet[i][1]*sigma_x - leftSet[i][0]*leftSet[i][1] - sigma_xy + leftSet[i][0]*sigma_y;
+        totalArea += temp;
+    }
     
 
-
+    //from right to left
+    for (j=rightSet.size(); j >=0; j--){
+        for (i=leftSet.size(); i >=0; i--){
+        cout << "left:(" << leftSet[i][0] << "," << leftSet[i][1] << ") right:(" << rightSet[j][0] << "," << rightSet[j][1] << endl;
+        sigma_x=0; sigma_y=0; sigma_xy=0; temp=0;
+        
+            if (rightSet[j][0] > leftSet[i][0] && rightSet[j][1] > leftSet[i][1]){
+                sigma_x+=rightSet[i][0];
+                sigma_y+=rightSet[i][1];
+                sigma_xy+=rightSet[i][0]*rightSet[i][1];
+            }
+        }
+        temp = (leftSet[j][1]*sigma_x - leftSet[j][0]*leftSet[j][1] - sigma_xy + leftSet[j][0]*sigma_y)*(-1);
+        totalArea += temp;
+    }
+    
 
 }
 
 
 
-int calArea(vector< vector<int> > pointSet, int front_idx, int end_idx, int &totalArea, 
-            int &sigma_x, int &sigma_y, int &sigma_xy){
+int calArea(vector< vector<int> > pointSet, int front_idx, int end_idx, int &totalArea){
     
     if (front_idx < end_idx){
 
-
         // Divide
         int mid_idx = (front_idx + end_idx)/2;
-        calArea(pointSet, front_idx, mid_idx, totalArea, sigma_x, sigma_y, sigma_xy);
-        calArea(pointSet, mid_idx+1, end_idx, totalArea, sigma_x, sigma_y, sigma_xy);
-
+        cout << front_idx << end_idx << mid_idx << endl;
+        calArea(pointSet, front_idx, mid_idx, totalArea);
+        calArea(pointSet, mid_idx+1, end_idx, totalArea);
         // conquer
-        crossArea(pointSet, front_idx, mid_idx, end_idx, totalArea,sigma_x, sigma_y, sigma_xy);
-        //totalArea = rightArea + leftArea + crossArea
+        crossArea(pointSet, front_idx, mid_idx, end_idx, totalArea);
+        
     }
     
 }
 
 
 int main(){ 
-    int n, i, j, value, deltaX, deltaY, totalArea=0, sigma_x=0, sigma_y=0, sigma_xy=0;
+    int n, i, j, value, deltaX, deltaY, totalArea=0;
     cin >> n;          
 
     vector<int> a_point;
@@ -69,17 +94,11 @@ int main(){
     //sort the pointSet by x coordinate.
     sort(pointSet.begin(), pointSet.end(), sortcol); 
 
-    cout << "after sorting: " << endl;
-    for (i=0; i<n; i++){
-        cout << pointSet[i][0] << " : " << pointSet[i][1] << endl;
+
+
+
+    calArea(pointSet, 0, n-1, totalArea);
     
-    }
-
-
-
-    //calArea(pointSet, 0, n-1, totalArea, sigma_x, sigma_y, sigma_xy);
-    
-    //cout << totalArea << endl;
-    //return totalArea; 
-    return 0;
+    cout << totalArea << endl;
+    return totalArea; 
 }
